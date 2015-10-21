26650cd701232218c21109f689dc5080457305ba
yofeca <jsonson@ubertor.com>
2015-10-21 09:02:02 +0800

    First commit

---
+++ eng-chinese.js
@@ -0,0 +1,39 @@
+<script>
+jQuery(document).ready(function(){
+jQuery('.buyers.Ideal.Location label').html('理想居住地');
+jQuery('.buyers.Price.Range label').html('价格区间');
+jQuery('.buyers.Type.Of.Home label').html('家庭类型');
+jQuery('.buyers.Name label').html('姓名');
+jQuery('.buyers.Email label').html('电子邮件');
+jQuery('.buyers.Phone label').html('电话');
+jQuery('.buyers.Mobile label').html('手机');
+jQuery('.buyers.Min.Bedrooms label').html('卧室数量(不小于)');
+jQuery('.buyers.Min.Bathrooms label').html('洗手间数量(不小于)');
+jQuery('.buyers.Preferred.Moving.Date label').html('预计搬家日期');
+jQuery('.buyers.Receive.Newsletter label').html('获得最新资讯');
+jQuery('.buyers.Comments label').html('留言');
+
+jQuery('.sellers.Address label').html('地址');
+jQuery('.sellers.City label').html('城市');
+jQuery('.sellers.Lot.Size label').html('土地面积');
+jQuery('.sellers.Moving\\/Selling label').html('搬家/出售');
+jQuery('.sellers.Name label').html('姓名');
+jQuery('.sellers.Email label').html('电子邮件');
+jQuery('.sellers.Phone label').html('电话');
+jQuery('.sellers.Mobile label').html('手机');
+jQuery('.sellers.Bedrooms label').html('卧室');
+jQuery('.sellers.Bathrooms label').html('洗手间');
+jQuery('.sellers.Finished.Area label').html('室内面积');
+jQuery('.sellers.Appliances.Included label').html('包含橱柜等设施');
+jQuery('.sellers.Price label').html('价格');
+jQuery('.sellers.Receive.Newsletter label').html('获得最新资讯');
+jQuery('.sellers.Comments label').html('留言');
+
+jQuery('.contact.Name label').html('姓名');
+jQuery('.contact.Email label').html('电子邮件');
+jQuery('.contact.Phone label').html('联系电话');
+jQuery('.contact.Comments label').html('意见留言');
+ 
+jQuery('.btn.submit').html('服从');
+});
+</script>
